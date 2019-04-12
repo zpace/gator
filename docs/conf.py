f43fa8838180181ee6a9cb1a448b5f3f21b70f3f
Zach Pace <zpace21@gmail.com>
2019-04-12 13:01:58 -0500

    v0.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Zach Pace'
 author = 'Zach Pace'
 
 # The short X.Y version
-version = ''
+version = '0.1'
 # The full version, including alpha/beta/rc tags
-release = '0.0'
+release = '0.1'
 
 
 # -- General configuration ---------------------------------------------------
