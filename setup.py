3de73157c445580e0faa385e3a200087caaa68f7
Zach Pace <zpace21@gmail.com>
2019-04-12 09:05:37 -0500

    first commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='gator',
+    version='0.0',
+    description='Linear-algebraic uncertainty propagation',
+    url='https://github.com/zpace/gator',
+    author='Zach Pace',
+    author_email='zpace@astro.wisc.edu',
+    license='MIT',
+    packages=['gator'],
+    zip_safe=False)
