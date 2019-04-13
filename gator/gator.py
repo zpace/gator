d6a42df37fea89264e4bc6a8cdb96a7df5114e48
Zach Pace <zpace21@gmail.com>
2019-04-13 11:57:12 -0500

    add broadcasted propagation

--- gator/gator.py
+++ gator/gator.py
@@ -41,3 +41,56 @@ def propagate_varmat(varmat, tmat):
     new_varmat = np.linalg.multi_dot([tmat.T, varmat, tmat])
 
     return new_varmat
+
+def propagate_varmats(varmats, tmat, axis=-1):
+    '''Propagate a series of variance matrices through a single linear transformation
+    
+    Executes vectorized pre- and post-multiplication of a variance-covariance
+    matrix by a transformation matrix
+    
+    Parameters
+    ----------
+    varmats : array-like
+        series of 2D variance-covariance matrix with `shape` `(n, n)`,
+        all stacked along one or more axes
+    tmat : array-like
+        2D transformation matrix with `shape` `(n, p)`, which transforms 
+        a vector of `len` `n` to a vector of `len` `p`
+    axis : {int, sequence}, optional
+        the axis (or axes) along with the matrix-multiplication is
+        broadcast (the default is -1, which means the variance matrices
+        are stacked along the final axis)
+    '''
+
+    if type(axis) is int:
+        axis = tuple(axis)
+    elif type(axis) in (tuple, list):
+        pass
+    else:
+        raise ValueError('axis must be integer or list/tuple')
+
+    if varmats.ndim <= 2:
+        raise TransformationError('variance matrix must be >2D')
+
+    if tmat.ndim != 2:
+        raise TransformationError('transformation matrix must be 2D')
+
+    num_broadcast_axes = len(axis)
+
+    # first, rearrange axes of varmats, s.t. broadcast axes end up first
+    varmats = np.moveaxis(varmats, axis, range(num_broadcast_axes))
+
+    if varmats.shape[num_broadcast_axes:][0] != varmats.shape[num_broadcast_axes:][1]:
+        raise TransformationError('variance matrix must be square')
+
+    if tmat.shape[0] != varmats.shape[:num_broadcast_axes][0]:
+        raise TransformationError('zeroth axis of transformation matrix must match zeroth axis of variance matrix')
+
+    new_varmats = np.einsum('ia,...ij,jb->...ab', tmat, varmats, tmat)
+
+    # rearrange result to have axes in the same order as the matrix
+    # that was originally passed
+    new_varmats = np.moveaxis(new_varmats, range(num_broadcast_axes), axis)
+
+    return new_varmats
+    
--- gator/tests/test_gator.py
+++ gator/tests/test_gator.py
@@ -22,7 +22,7 @@ class TestPropagateVarmat(TestCase):
         with self.assertRaises(gator.TransformationError):
             varmat_final = gator.propagate_varmat(self.varmat_orig, self.tmat[:, 0])
 
-    def test_requires_2dsquarevarmat(self):
+    def test_requires_correctshapevarmat(self):
         with self.assertRaises(gator.TransformationError):
             varmat_final = gator.propagate_varmat(self.varmat_orig[0, :], self.tmat)
             varmat_final = gator.propagate_varmat(self.varmat_orig[:, 0], self.tmat)
@@ -33,3 +33,48 @@ class TestPropagateVarmat(TestCase):
     def test_requires_varmatliketmat(self):
         with self.assertRaises(gator.TransformationError):
             varmat_final = gator.propagate_varmat(self.varmat_orig[:-1, :-1], self.tmat)
+
+class TestPropagateVarmats(TestPropagateVarmat):
+    dim_orig = 3
+    dim_final = 2
+    start_arr = np.ones(2)
+    tmat = np.ones((dim_orig, dim_final))
+    varmat_orig = np.eye(dim_orig)
+    broadcast_axis = (2, 3)
+    n_within_broadcast = (3, 4)
+    varmat_orig_large = np.tile(
+        varmat_orig[..., None, None], (1, 1) + n_within_broadcast)
+
+    def test_is_array(self):
+        varmat_final = gator.propagate_varmats(
+            self.varmat_orig_large, self.tmat, axis=self.broadcast_axis)
+        self.assertTrue(isinstance(varmat_final, np.ndarray))
+
+    def test_has_correct_dimension(self):
+        varmat_final = gator.propagate_varmats(
+            self.varmat_orig_large, self.tmat, axis=self.broadcast_axis)
+        self.assertTrue(varmat_final.shape == (self.dim_final, self.dim_final) + self.n_within_broadcast)
+
+    def test_requires_2dtmat(self):
+        with self.assertRaises(gator.TransformationError):
+            varmat_final = gator.propagate_varmats(
+                self.varmat_orig_large, self.tmat[:, 0], axis=self.broadcast_axis)
+
+    def test_requires_correctshapevarmat(self):
+        with self.assertRaises(gator.TransformationError):
+            varmat_final = gator.propagate_varmats(
+                self.varmat_orig_large[:1, :, ...], self.tmat, axis=self.broadcast_axis)
+            varmat_final = gator.propagate_varmats(
+                self.varmat_orig_large[:, :1, ...], self.tmat, axis=self.broadcast_axis)
+            varmat_final = gator.propagate_varmats(
+                self.varmat_orig_large[:1, :1, ...], self.tmat, axis=self.broadcast_axis)
+            varmat_final = gator.propagate_varmats(
+                self.varmat_orig_large[:-1, :, ...], self.tmat, axis=self.broadcast_axis)
+            varmat_final = gator.propagate_varmats(
+                self.varmat_orig_large[:, :-1, ...], self.tmat, axis=self.broadcast_axis)
+
+        def test_requires_varmatliketmat(self):
+            with self.assertRaises(gator.TransformationError):
+                varmat_final = gator.propagate_varmats(
+                    self.varmat_orig_large[:-1, :-1, ...], self.tmat,
+                    axis=self.broadcast_axis)
