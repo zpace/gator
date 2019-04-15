23f51d90d3fbd364293e483cf020e23280d8bfa4
Zach Pace <zpace21@gmail.com>
2019-04-15 15:35:45 -0500

    fix cov docs

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ copyright = '2019, Zach Pace'
 author = 'Zach Pace'
 
 # The short X.Y version
-version = '0.1'
+version = '0.1.1'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
